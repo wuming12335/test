5dff0387f9956acca31704189d6dab6d16586564
wuming12335 <499707547@qq.com>
2020-01-06 10:38:10 +0800

    add a  method

--- src/demo/Test01.java
+++ src/demo/Test01.java
@@ -4,5 +4,11 @@ public class Test01 {
 
 	public static void main(String[] args) {
 		System.out.println("hello world");
+		
+		System.out.println("a+b="+add(1, 2));
+	}
+	
+	public static int add(int a, int b){
+		return a+b;
 	}
 }
