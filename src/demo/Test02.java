652e072c92793cb9f8758912ba9ead2df2af6698
wuming12335 <499707547@qq.com>
2020-01-06 10:50:43 +0800

    add Test02.java

---
+++ src/demo/Test02.java
@@ -0,0 +1,13 @@
+package demo;
+
+public class Test02 {
+	
+	public static int sub(int a, int b){
+		return a-b;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("a-b="+sub(2, 1));
+	}
+
+}
