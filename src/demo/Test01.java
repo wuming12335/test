045a71540485464fa37d27914b6116d2b1f16f89
wuming12335 <499707547@qq.com>
2020-01-07 11:22:54 +0800

    add  new

--- src/demo/Test01.java
+++ src/demo/Test01.java
@@ -3,7 +3,7 @@ package demo;
 public class Test01 {
 
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		System.out.println("hello world111");
 		
 		System.out.println("a+b="+add(1, 2));
 		System.out.println("123123");
